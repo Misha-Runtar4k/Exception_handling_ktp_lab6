8c7b959eace35085007e0a38ccf2ee4eac6f9a70
Misha-Runtar4k <misabuc81@gmail.com>
2025-10-15 17:26:12 +0300

    Add files via upload

---
+++ lab6 Ktp Buts/lab6 Ktp Buts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab6_Ktp_Buts
+{
+    class Dogs
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("Введіть число: ");
+                string gav = Console.ReadLine();
+
+                try
+                {
+                    double numberdouble = double.Parse(gav);
+                    int numberint = (int)numberdouble;
+                    Console.WriteLine($"Круто, ти ввів число: {numberint}");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bсе фігня давай по новому;)  ");
+                }
+            }
+        }
+    }
+}
